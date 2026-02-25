69a9eaa60a61d31d96f582760b5033d13929955c
Zimeng Yang <zimengyang8@gmail.com>
2026-02-25 17:32:06 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Zimeng Yang on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
